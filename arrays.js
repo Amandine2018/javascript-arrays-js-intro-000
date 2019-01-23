fe41be7f141cea9bd33da133da04392bfba2a9cc
blithe-database-2858 <amavelamala@gmail.com>
2019-01-23 15:38:28 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -19,3 +19,21 @@ function destructivelyAddElementToEndOfArray(array, element) {
   array.push(elemnent);
   return array;
 }
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array.slice(1);
+  return array;
+}
+ function destructivelyRemoveElementFromEndOfArray(array) {
+   array.pop();
+   return array;
+ }
